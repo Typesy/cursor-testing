fb4395c27f8d8c8a878c2fc5a53df8be23fe7562
Typesy <tonykrem55@gmail.com>
2025-03-20 23:25:08 -0700

    Removed .next from Git tracking

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a9c1d11016a19ab8"; }
+/******/ 		__webpack_require__.h = function() { return "427bdd7fde488732"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
